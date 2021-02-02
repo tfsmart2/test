b6ee26d1db907976d592f62689879977165ca870
tfsmart2 <74022167+tfsmart2@users.noreply.github.com>
2021-02-02 18:45:15 +0100

    Update tronFlow.js

--- assets/js/tronFlow.js
+++ assets/js/tronFlow.js
@@ -331,7 +331,7 @@ const contractData = () => {
 
 const loadContract = async () => {
   contract = await customTronWeb.contract().at(contractAddress);
-  startInterval(30, contractData);
+  startInterval(1, contractData);
 };
 
 const loadNewContract = async () => {
